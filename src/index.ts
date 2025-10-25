ad45089c1ce98161b25f102f23ee4f455da69e5c
madkarmaa <madkarma99@gmail.com>
2025-10-25 19:48:39 +0200

    fix: join path to enable importing module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { glob } from 'node:fs/promises';
+import { join } from 'node:path';
 import { Command } from 'commander';
 import pkg from '../package.json';
 import { relativeToMe } from './utils';
@@ -12,8 +13,8 @@ program
     .version(pkg.version);
 
 const loadCommands = async () => {
-    for await (const file of glob('*.ts', { cwd: COMMANDS_DIR })) {
-        const { default: command } = await import(file);
+    for await (const file of glob('**/*.ts', { cwd: COMMANDS_DIR })) {
+        const { default: command } = await import(join(COMMANDS_DIR, file));
         if (!command || !(command instanceof Command)) continue;
         program.addCommand(command);
     }
